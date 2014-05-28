47762976ab1048b227c45fb2359c31c748b45fa1
Yuval Adam <yuv.adm@gmail.com>
2014-05-28 14:11:24 +0300

    GET and POST data

--- server.py
+++ server.py
@@ -1,12 +1,22 @@
 from flask import Flask
-from flask import render_template
-app = Flask(__name__)
+from flask import render_template, request
+from unipath import FSPath as Path
 
+DATA_DIR = Path(__file__).absolute().child('data')
+
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
+@app.route('/data', methods=['POST', 'GET'])
+def data():
+    if request.method == 'GET':
+        return 'hai'
+    elif request.method == 'POST':
+        print request.json
+        return 'ok'
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
